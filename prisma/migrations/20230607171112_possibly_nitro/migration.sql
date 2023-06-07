f291bb4e2ae4ec5d22c6af996166e15e95a82f49
Hampus Kraft <hello@hampuskraft.com>
2023-06-08 01:56:54 +0200

    [skip ci] Remove migration

--- prisma/migrations/20230607171112_possibly_nitro/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "pomelos" ADD COLUMN     "possibly_nitro" BOOLEAN NOT NULL DEFAULT false;
-
--- CreateIndex
-CREATE INDEX "pomelos_possibly_nitro_idx" ON "pomelos"("possibly_nitro");
