fc43fa8a5ecfb6c0e1bf45f75af453ec13767532
Hampus Kraft <hello@hampuskraft.com>
2023-06-08 14:25:01 +0200

    Oops

--- src/app/oauth2/page.tsx
+++ src/app/oauth2/page.tsx
@@ -5,6 +5,6 @@ export const revalidate = 60;
 
 export default async function OAuth2() {
   const botStats = await getBotStats();
-  const pomeloStats = await getPomeloStats();
+  const pomeloStats = await getPomeloStats({oauth2: true});
   return <Landing botStats={botStats} pomeloStats={pomeloStats} isOAuth2 />;
 }
