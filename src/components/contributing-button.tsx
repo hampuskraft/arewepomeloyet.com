a366664f58da3fa6811aee49c638310f4a0ea659
Hampus Kraft <hello@hampuskraft.com>
2023-06-09 23:25:41 +0200

    Push #contributing hash to history state

--- src/components/contributing-button.tsx
+++ src/components/contributing-button.tsx
@@ -17,7 +17,10 @@ export default function ContributingButton() {
     <a
       className="max-w-max rounded-xl bg-blue-500 px-4 py-2 font-display font-bold text-white transition-colors duration-200 ease-in-out hover:bg-blue-600"
       role="button"
-      onClick={() => smoothScrollTo('#contributing')}
+      onClick={() => {
+        smoothScrollTo('#contributing');
+        window.history.pushState({}, '', '#contributing');
+      }}
     >
       How does this site work, and how can I help?
     </a>
